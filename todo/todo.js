5bd3ea5a042efc4ef05ffbfc851cd8fb2fa47d21
Matheus <matheusmdev1@gmail.com>
2025-02-23 17:14:50 -0300

    todo

---
+++ todo/todo.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+// Configuração do banco de dados MySQL
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root', // Altere para seu usuário do MySQL
+  password: '33369333!', // Altere para sua senha do MySQL
+  database: 'todo_db'
+});
+
+db.connect(err => {
+  if (err) {
+    console.error('Erro ao conectar ao MySQL:', err);
+  } else {
+    console.log('Conectado ao MySQL');
+  }
+});
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Rota para obter todas as tarefas
+app.get('/tasks', (req, res) => {
+  db.query('SELECT * FROM tasks', (err, results) => {
+    if (err) return res.status(500).send(err);
+    res.json(results);
+  });
+});
+
+// Rota para adicionar uma nova tarefa
+app.post('/tasks', (req, res) => {
+  const { title } = req.body;
+  db.query('INSERT INTO tasks (title) VALUES (?)', [title], (err, result) => {
+    if (err) return res.status(500).send(err);
+    res.status(201).json({ id: result.insertId, title, completed: false });
+  });
+});
+
+// Rota para marcar uma tarefa como concluída
+app.put('/tasks/:id', (req, res) => {
+  const { id } = req.params;
+  const { completed } = req.body;
+  db.query('UPDATE tasks SET completed = ? WHERE id = ?', [completed, id], (err) => {
+    if (err) return res.status(500).send(err);
+    res.json({ message: 'Tarefa atualizada com sucesso' });
+  });
+});
+
+// Rota para deletar uma tarefa
+app.delete('/tasks/:id', (req, res) => {
+  const { id } = req.params;
+  db.query('DELETE FROM tasks WHERE id = ?', [id], (err) => {
+    if (err) return res.status(500).send(err);
+    res.json({ message: 'Tarefa removida com sucesso' });
+  });
+});
+
+// Iniciando o servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
